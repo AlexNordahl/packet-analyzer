dba479fc8bd66cdfc35c4424ff576e3d788698f3
AlexNordahl <nordahlalex@gmail.com>
2025-12-09 14:57:19 +0100

    moved printer functions to printer folder

---
+++ src/printer/printer.cpp
@@ -0,0 +1,371 @@
+#include "printer.h"
+#include "ansi_colors.h"
+
+using std::setw;
+using std::left;
+
+void printEthernetFrame(const EtherFrame &frame)
+{
+    const auto protocolColor = ansi::bright_magenta;
+    const auto fieldsColor = ansi::bright_blue;
+    const auto valuesColor = ansi::white;
+
+    std::cout << "┌───────────────────────────────────────────────────┐\n";
+    std::cout << "│ ";
+
+    std::cout << protocolColor
+              << left << setw(49) << "ETHERNET" 
+              << ansi::reset << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(17) << "DST"
+              << left << setw(19) << "SRC"
+              << left << setw(9) << "TYPE"
+              << left << "DATA"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(17) << frame.destStr()
+              << left << setw(19) << frame.sourceStr()
+              << left << setw(9) << frame.typeStr()
+              << left << setw(4) << frame.getPayloadLen()
+              << ansi::reset
+              << " │\n";
+        
+    std::cout << "├───────────────────────────────────────────────────┤\n";
+}
+
+void printIPV4(const IpHeader& header)
+{
+    const auto protocolColor = ansi::bright_cyan;
+    const auto fieldsColor = ansi::bright_blue;
+    const auto valuesColor = ansi::white;
+
+    std::cout << "│ ";
+    std::cout << protocolColor
+              << left << setw(49) << "IP" 
+              << ansi::reset << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(10) << "VERSION"
+              << left << setw(10) << "IHL"
+              << left << setw(10) << "TOS"
+              << left << setw(19) << "TOTAL LENGTH"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(10) << header.versionStr()
+              << left << setw(10) << header.strIHL()
+              << left << setw(10) << "---"
+              << left << setw(19) << header.totalLength()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(20) << "IDENTIFICATION"
+              << left << setw(10) << "FLAGS"
+              << left << setw(19) << "FRAGMENT OFFSET"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(20) << header.id()
+              << left << setw(10) << header.flags()
+              << left << setw(19) << header.fragOffset()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(20) << "TIME TO LIVE"
+              << left << setw(10) << "PROTOCOL"
+              << left << setw(19) << "CHECKSUM"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(20) << header.getTTL()
+              << left << setw(10) << protocolKeyword(header.getProtocol())
+              << left << setw(19) << header.getChecksum()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(20) << "SOURCE ADDRESS"
+              << left << setw(29) << "DESTINATION ADDRESS"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(20) << header.srcStr()
+              << left << setw(29) << header.dstStr()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "├───────────────────────────────────────────────────┤\n";
+    
+}
+
+void printTCP(const TcpHeader& header)
+{
+    const auto protocolColor = ansi::bright_red;
+    const auto fieldsColor = ansi::bright_blue;
+    const auto valuesColor = ansi::white;
+
+    std::cout << "│ ";
+    std::cout << protocolColor
+              << left << setw(49) << "TCP" 
+              << ansi::reset << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "SOURCE PORT"
+              << left << setw(25) << "DESTINATION PORT"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.sourcePort()
+              << left << setw(25) << header.destPort()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "SEQUENCE NUMBER"
+              << left << setw(25) << "ACK NUMBER"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.seq()
+              << left << setw(25) << header.ack()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "HDR LEN (bytes)"
+              << left << setw(25) << "FLAGS"
+              << ansi::reset
+              << " │\n";
+
+    const auto flags = tcpFlags(header.flags);
+    std::string firstLine {};
+    std::string secondLine {};
+    for (size_t i = 0; i < flags.size(); ++i)
+    {
+        if (i >= 4)
+        {
+            if (!secondLine.empty())
+                secondLine += "|";
+            secondLine += flags.at(i);
+            continue;
+        }
+
+        if (!firstLine.empty())
+            firstLine += "|";
+        firstLine += flags.at(i);
+    }
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.headerLengthBytes()
+              << left << setw(25) << firstLine
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "└───────────────────────────────────────────────────┘\n";
+}
+
+void printUDP(const UdpHeader& header)
+{
+    const auto protocolColor = ansi::bright_yellow;
+    const auto fieldsColor = ansi::bright_blue;
+    const auto valuesColor = ansi::white;
+
+    std::cout << "│ ";
+    std::cout << protocolColor
+              << left << setw(49) << "UDP" 
+              << ansi::reset << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "SOURCE PORT"
+              << left << setw(25) << "DESTINATION PORT"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.sourcePort()
+              << left << setw(25) << header.destPort()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "LENGTH"
+              << left << setw(25) << "CHECKSUM"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.length()
+              << left << setw(25) << header.checksum()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "├───────────────────────────────────────────────────┤\n";
+}
+
+void printICMP(const IcmpHeader& header)
+{
+    const auto protocolColor = ansi::bright_cyan;
+    const auto fieldsColor = ansi::bright_blue;
+    const auto valuesColor = ansi::white;
+
+    std::cout << "│ ";
+    std::cout << protocolColor
+              << left << setw(49) << "ICMP" 
+              << ansi::reset << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(20) << "TYPE"
+              << left << setw(20) << "CODE"
+              << left << setw(9) << "CHECKSUM"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(20) << header.getType()
+              << left << setw(20) << header.getCode()
+              << left << setw(9) << header.getChecksum()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(20) << "IDENTIFIER"
+              << left << setw(29) << "SEQUENCE"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(20) << header.getIdentifier()
+              << left << setw(29) << header.getSequence()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "└───────────────────────────────────────────────────┘\n";
+}
+
+void printDNS(const DnsHeader& header)
+{
+    const auto protocolColor = ansi::bright_green;
+    const auto fieldsColor = ansi::bright_blue;
+    const auto valuesColor = ansi::white;
+
+    std::cout << "│ ";
+    std::cout << protocolColor
+              << left << setw(49) << "DNS" 
+              << ansi::reset << " │\n";
+    
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "ID"
+              << left << setw(25) << "FLAGS"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+            << std::left << std::setw(24) << header.getId()
+            << ansi::reset;
+
+    const auto flags = dnsFlags(header.getFlags());
+
+    std::size_t counter = 0;
+    std::string buffer;
+    bool firstLine = true;
+
+    for (std::size_t i = 0; i < flags.size(); ++i)
+    {
+        if (!buffer.empty())
+            buffer += '|';
+
+        buffer += flags[i];
+        ++counter;
+
+        const bool lastItem = (i == flags.size() - 1);
+
+        if (counter == 3 or lastItem)
+        {
+            if (firstLine)
+            {
+                std::cout << valuesColor;
+                std::cout << std::left << std::setw(25) << buffer
+                << ansi::reset << " │\n";
+                firstLine = false;
+            }
+            else
+            {
+                std::cout << "│ ";
+                std::cout << valuesColor
+                          << std::left << std::setw(24) << ""
+                          << std::left << std::setw(25) << buffer 
+                          << ansi::reset << " │\n";
+            }
+
+            buffer.clear();
+            counter = 0;
+        }
+    }
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "QUESTION COUNT"
+              << left << setw(25) << "ANSWER COUNT"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.getQuestionCount()
+              << left << setw(25) << header.getAnswerCount()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << fieldsColor
+              << left << setw(24) << "AUTH COUNT"
+              << left << setw(25) << "ADDITIONAL COUNT"
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "│ ";
+    std::cout << valuesColor
+              << left << setw(24) << header.getAuthCount()
+              << left << setw(25) << header.getAdditionalCount()
+              << ansi::reset
+              << " │\n";
+
+    std::cout << "└───────────────────────────────────────────────────┘\n";
+}
---
+++ src/printer/printer.h
@@ -0,0 +1,22 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+#include "../net_headers/ether_frame.h"
+#include "../net_headers/ip_header.h"
+#include "../net_headers/arp_header.h"
+#include "../net_headers/tcp_header.h"
+#include "../net_headers/udp_header.h"
+#include "../net_headers/icmp_header.h"
+#include "../net_headers/dns_header.h"
+#include "../helpers/readable_protocols.h"
+#include <iomanip>
+#include <iostream>
+
+void printEthernetFrame(const EtherFrame& frame);
+void printIPV4(const IpHeader& header);
+void printTCP(const TcpHeader& header);
+void printUDP(const UdpHeader& header);
+void printICMP(const IcmpHeader& header);
+void printDNS(const DnsHeader& header);
+
+#endif
